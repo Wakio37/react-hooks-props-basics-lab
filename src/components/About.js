3f0dc8dc1e9243b8ca4d0cef1364815ebdf9c3d7
Peris Wakio <peris.kingori@student.moringaschool.com>
2023-08-29 19:19:41 +0000

    completed asignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,24 @@
 import React from "react";
 
-function About() {
+function Links(props){
+return(
+  <div>
+    <h3>Links</h3>
+    <a href= {props.linkedin}>Linkedin</a>
+    <a href= {props.github}>Github</a>
+  </div>
+
+)
+  
+}
+
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{props.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      <Links linkedin={props.links.linkedin} github={props.links.github}/> 
     </div>
   );
 }
